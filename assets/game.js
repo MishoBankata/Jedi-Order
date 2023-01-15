13f3cd9ccf4861b58b50ae1c35bde356b051a796
Mihail <mb2004@abv.bg>
2023-01-15 18:54:26 +0200

    added matrix with creatures and terrain; console displays creature/terrain type when player is in a certain position

--- assets/game.js
+++ assets/game.js
@@ -7,8 +7,8 @@ let sqY = 50;
 context.fillStyle = "red";
 context.strokeStyle = "yellow";
 
-let heroPosX = 0;
-let heroPosY = 0;
+let heroPosX = Number(0);
+let heroPosY = Number(0);
 
 
 let img = new Image();
@@ -17,7 +17,51 @@ img.onload = function(){
 	map();
 }
 
+let creatures = [
+	[0, 0, 0, 0 ,0, 0, 0],
+	[0, 0, 0, 0 ,0, 0, 0],
+	[0, 0, 0, 0 ,0, 0, 0],
+	[0, 0, 0, 0 ,0, 0, 0],
+	[0, 0, 0, 0 ,0, 0, 0],
+	[0, 0, 0, 0 ,0, 0, 0],
+	[0, 0, 0, 0 ,0, 0, 0]
+];
 
+for(let i = 0; i < canvas.height / sqY; i++) {
+	for(let j = 0; j < canvas.width / sqX; j++) {
+		context.strokeRect(sqX * j, sqY * i, sqX, sqY);
+		creatures[i][j] = Math.floor(Math.random() * 100);
+	}
+}
+
+function creatureCheck(){
+	let i = Number(heroPosX / 50);
+	let j = Number(heroPosY / 50);
+	if(creatures[i][j] <= 1) {
+		console.log("gorgodon");
+	} else if (creatures[i][j] <= 9){
+		console.log("asharl panther");
+	} else if (creatures[i][j] <= 23){
+		console.log("razhak");
+	} else if (creatures[i][j] <= 39){
+		console.log("lisk");
+	} else if (creatures[i][j] <= 59){
+		console.log("crystal caves");
+	} else if (creatures[i][j] <= 79){
+		console.log("stalagmites");
+	} else console.log("cliffs");
+}
+
+//        CREATURES 40%
+//        0-1 gorgodon 2%
+//        2-9 asharl panther 8%
+//        10-23 razhak 14%
+//        24-39 lisk 16%
+//        
+//        TERRAIN 60%
+//        40-59 crystal caves 20%
+//        60-79 stalagmites 20%
+//        80-99 cliffs 20%
 
 function map() {
 	context.clearRect(0, 0, canvas.width, canvas.height);
@@ -25,7 +69,6 @@ function map() {
 	for(let i = 0; i < canvas.height / sqY; i++) {
 		for(let j = 0; j < canvas.width / sqX; j++) {
 			context.strokeRect(sqX * j, sqY * i, sqX, sqY);
-			
 		}
 	}
 }
@@ -37,6 +80,7 @@ function up() {
 	}
 	heroPosY -= 50;
 	map();
+	creatureCheck()
 }
 
 function down() {
@@ -46,6 +90,7 @@ function down() {
 	}
 	heroPosY += 50;
 	map();
+	creatureCheck()
 }
 
 function left() {
@@ -55,6 +100,7 @@ function left() {
 	}
 	heroPosX -= 50;
 	map();
+	creatureCheck()
 }
 
 function right() {
@@ -64,4 +110,5 @@ function right() {
 	}
 	heroPosX += 50;
 	map();
+	creatureCheck()
 }
